922ac3c71d3d564b9b40e9ace9ea993dbb66008e
Dennis Reimann <mail@dennisreimann.de>
2022-10-28 17:58:14 +0200

    Markup and JS cleanups

--- pug.config.js
+++ pug.config.js
@@ -1,9 +1,6 @@
 // initialize markdown rendering
 const renderMarkdown = require('./markdown')
 
-const slugify = str => str.toLowerCase().replace(/\W/, '-')
-const random = max =>  Math.floor(Math.random() * Math.floor(max))
-const linkTarget = url => url.startsWith('http') ? '_blank' : null
 const assetPath = path => {
   let revs
   try { revs = require('./rev-manifest.json') } catch (error) { }
@@ -16,10 +13,7 @@ const assetUrl = (path, protocol = 'https') => {
 module.exports = {
   basedir: './src/pages/includes',
   siteData: require('./site-data'),
-  random,
-  slugify,
   assetUrl,
   assetPath,
-  linkTarget,
   renderMarkdown,
 }
